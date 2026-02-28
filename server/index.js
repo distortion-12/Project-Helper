4c13dd13056f4d03f1093edb04f60b2343d2d666
distortion-12 <your-ramchouhan045@gmail.com>
2026-02-28 19:36:03 +0530

    Updates

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    'https://project-nexus-ula.vercel.app' // <-- your Vercel frontend
+    'https://project-nexus-wala.vercel.app/' // <-- your Vercel frontend
   ],
   credentials: true
 }));
